0481f3abbba2b9889709b2319b4c04fb4c5d2418
Conrad <conradkaranja5@gmail.com>
2026-06-22 12:39:20 +0300

    Create structural tables in database.py

--- database.py
+++ database.py
@@ -9,3 +9,59 @@ def get_db_connection():
     conn.row_factory = sqlite3.Row
     return conn
 
+def initialize_database():
+    #Creates the structural tables and seeds initial mock data if tables are empty.
+    conn = get_db_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("PRAGMA foreign_keys = ON;")
+
+    cursor.execute("""
+                   CREATE TABLE IF NOT EXISTS tariffs
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    hospital_name TEXT NOT NULL,
+                    procedure_name TEXT NOT NULL,
+                    tariff_cap REAL NOT NULL,
+                    UNIQUE(hospital_name, procedure_name)
+                  """)
+
+    # 3. Create Members Table (Links to Policies via Foreign Key)
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS members (
+        member_id TEXT PRIMARY KEY,
+        name TEXT NOT NULL,
+        remaining_balance REAL NOT NULL,
+        policy_tier TEXT NOT NULL,
+        FOREIGN KEY (policy_tier) REFERENCES policies(policy_tier)
+    );
+    """)
+
+
+    # 4. Create Hospital Contract Tariffs Table
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS tariffs (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        hospital_name TEXT NOT NULL,
+        procedure_name TEXT NOT NULL,
+        tariff_cap REAL NOT NULL,
+        UNIQUE(hospital_name, procedure_name)
+    );
+    """)
+
+    # 5. Create Authorization Transaction Ledger Table
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS authorization_ledger (
+        transaction_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        member_id TEXT NOT NULL,
+        hospital_name TEXT NOT NULL,
+        procedure_name TEXT NOT NULL,
+        proposed_cost REAL NOT NULL,
+        allowed_amount REAL NOT NULL,
+        overcharge_blocked REAL NOT NULL,
+        insurer_liability REAL NOT NULL,
+        patient_copay REAL NOT NULL,
+        status TEXT NOT NULL,
+        timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+        FOREIGN KEY (member_id) REFERENCES members(member_id)
+    );
+    """)
