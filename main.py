7a9fffbae389bfb54d864bd5aaffad902cd5e4cd
Conrad <conradkaranja5@gmail.com>
2026-06-23 09:51:50 +0300

    Add try and except block to catch errors

--- main.py
+++ main.py
@@ -4,3 +4,11 @@ import os
 
 import database as db
 from app_ui import MedAuthApp
+
+try:
+     db.initialize_database()
+
+except Exception as e:
+ print(f"Database Initialization Failed: {e}")
+ print("Aborting application startup sequence.")
+ sys.exit(1)
