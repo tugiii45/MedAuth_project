cae3153a493b198d79dc79dabfb48bfc9ac2e019
Conrad <conradkaranja5@gmail.com>
2026-06-24 10:05:05 +0300

    implement secure authentication window and base app layout

--- app_ui.py
+++ app_ui.py
@@ -1,1 +1,27 @@
-import customtkinter as ctk; MedAuthApp = ctk.CTk
+import tkinter as tk
+import customtkinter as ctk
+import threading
+import requests
+from database import lookup_member_data, lookup_tariff_rate, log_transaction, verify_user
+
+class LoginWindow(ctk.CTkToplevel):
+    def __init__(self, on_success):
+        super().__init__()
+        self.title("MedAuth Security")
+        self.geometry("320x240")
+        self.grab_set()
+
+        ctk.CTkLabel(self, text="System Authentication", font=("Arial", 16, "bold")).pack(pady=20)
+        self.ent_user = ctk.CTkEntry(self, placeholder_text="Username")
+        self.ent_user.pack(pady=5)
+        self.ent_pass = ctk.CTkEntry(self, placeholder_text="Password", show="*")
+        self.ent_pass.pack(pady=5)
+        ctk.CTkButton(self, text="Login", command=self.check_login).pack(pady=15)
+        self.on_success = on_success
+
+    def check_login(self):
+        if verify_user(self.ent_user.get(), self.ent_pass.get()):
+            self.on_success()
+            self.destroy()
+        else:
+            self.title("Access Denied!")
